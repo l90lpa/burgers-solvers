9eea2ddfc082f2440a76d159277edd903e26ff21
Liam Adams <liam1540@googlemail.com>
2023-08-23 17:06:54 -0400

    add advection solver using jax

---
+++ solvers-jax/advection-jax.py
@@ -0,0 +1,80 @@
+from jax import config, vjp, jvp, jit
+config.update("jax_enable_x64", True)
+import jax.numpy as jnp
+from math import pi, ceil
+from functools import partial
+import numpy as np
+import matplotlib.pyplot as plt
+
+@partial(jit, static_argnames=['f'])
+def RK3_step(f, y, dt):
+    k1 = f(y)
+    k2 = f(y + dt * 0.5 * k1)
+    k3 = f(y + dt * 0.75 * k2)
+
+    return (1.0 / 9.0) * (2.0 * k1 + 3.0 * k2 + 4.0 * k3)
+
+def RK3(f, y0, dt, num_steps):
+    y = y0
+    
+    for i in range(num_steps):
+        y = y + dt * RK3_step(f, y, dt)
+
+    return y
+
+def create_advection_opeartor(dx, n):
+    
+    @jit
+    def advection_op(u):
+        dx2 = dx * 2
+        u_new = jnp.empty_like(u)
+        u_new = u_new.at[0].set((u[1] - u[n-1]) / dx2)
+        for i in range(1, n-1):
+            u_new = u_new.at[i].set((u[i+1] - u[i-1]) / dx2)
+        u_new = u_new.at[n-1].set((u[0] - u[n-2]) / dx2)
+        return u_new
+    
+    return advection_op
+
+def solver(u_initial, v, dx, num_points, dt, num_steps):
+    A = create_advection_opeartor(dx, num_points)
+
+    @jit
+    def f(u):
+        return - v * A(u)
+    
+    u_end = RK3(f, u_initial, dt, num_steps)
+
+    return u_end
+    
+
+# Example usage
+if __name__ == "__main__":
+    # Define problem parameters
+    num_points = 100
+    domain_length = 1.0
+    dx = domain_length / num_points
+    v = 1.2
+    C = 0.1 # Courant number
+    dt = (dx / v) * C  # Time step size
+    num_steps = ceil(1.0 * (domain_length / (dt * v)))
+
+    # Create the initial velocity profile (e.g., a sinusoidal profile)
+    x = jnp.linspace(0, domain_length - dx, num_points)
+    u_initial = jnp.sin((2*pi/domain_length) * x)
+
+    solver_partial = lambda u0 : solver(u0, v, dx, num_points, dt, num_steps)
+    
+    # Solve the Burgers' equation
+    u_final = solver_partial(u_initial)
+
+    # Plot the results
+    plt.plot(x, u_initial, label="Initial Condition")
+    plt.plot(x, u_final, label=f"Solution after {num_steps} time steps")
+    plt.xlabel("Position (x)")
+    plt.ylabel("Solution (u)")
+    plt.legend()
+    plt.show()
+
+
+
