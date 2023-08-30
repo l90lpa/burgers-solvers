fa48d7dd43b56d6fb6ff4a3a30b3fac5d0a36baf
Liam Adams <liam1540@googlemail.com>
2023-08-30 17:25:45 -0400

    start work on TLM and ADM for ab3 and ab3 based advection solver

--- solvers-jax/ab3.py
+++ solvers-jax/ab3.py
@@ -8,38 +8,141 @@ from checkpoint import reverseLoopCheckpointed
 def AB3_step(f, y, y1, y2):
     return (1.0 / 12.0) * (23.0 * f(y) - 16.0 * f(y1) + 5.0 * f(y2))
 
+
 def AB3(f, y0, y1, y2, dt, num_steps):
 
     y_prev_2 = y2
     y_prev_1 = y1
-    y = y0
+    y_prev_0 = y0
 
     for i in range(2, num_steps):
-        y_new = y + dt * AB3_step(f, y, y_prev_1, y_prev_2)
+        y_new = y_prev_0 + dt * AB3_step(f, y_prev_0, y_prev_1, y_prev_2)
 
         y_prev_2 = y_prev_1
-        y_prev_1 = y
-        y = y_new
+        y_prev_1 = y_prev_0
+        y_prev_0 = y_new
 
-    return y
+    return y_prev_0
 
-def AB3_tlm(f, y0, dy0, y1, y2, dt, num_steps):
 
+def AB3_tlm(f, y0, y1, y2, dy0, dy1, dy2, dt, num_steps):
+
+    @jit
+    def step(y_prev_0, y_prev_1, y_prev_2):
+        return y_prev_0 + dt * AB3_step(f, y_prev_0, y_prev_1, y_prev_2)
+
+    dy_prev_2 = dy2
     y_prev_2 = y2
+
+    dy_prev_1 = dy1
     y_prev_1 = y1
     
-    dy = dy0
-    y = y0
-
-    def step(y):
-        return y + dt * AB3_step(f, y, y_prev_1, y_prev_2)
+    dy_prev_0 = dy0
+    y_prev_0 = y0
 
     for i in range(2, num_steps):
         # y_new = y + dt * AB3_step(f, y, y_prev_1, y_prev_2)
-        y_new, dy_new = jvp(step, y, dy)
+        y_new, dy_new = jvp(step, (y_prev_0, y_prev_1, y_prev_2), (dy_prev_0, dy_prev_1, dy_prev_2))
+
+        dy_prev_2 = dy_prev_1
+        y_prev_2 = y_prev_1
+        dy_prev_1 = dy_prev_0
+        y_prev_1 = y_prev_0
+        dy_prev_0 = dy_new
+        y_prev_0 = y_new
+
+    return y_prev_0, dy_prev_0
+
+
+def AB3_adm(f, y0, y1, y2, Dy, dt, num_steps):
+
+    @jit
+    def step(y_prev_0, y_prev_1, y_prev_2):
+        return y_prev_0 + dt * AB3_step(f, y_prev_0, y_prev_1, y_prev_2)
+    
+    y_prev_2_cache = jnp.zeros((num_steps, jnp.size(y0)), dtype=y0.dtype)
+    y_prev_1_cache = jnp.zeros_like(y_prev_2_cache)
+    y_prev_0_cache = jnp.zeros_like(y_prev_2_cache)
+    y_prev_2_cache = y_prev_2_cache.at[0, :].set(y2)
+    y_prev_1_cache = y_prev_1_cache.at[0, :].set(y1)
+    y_prev_0_cache = y_prev_0_cache.at[0, :].set(y0)
+
+    y_prev_2 = y2
+    y_prev_2_cache = y_prev_2_cache.at[1, :].set(y_prev_2)
+    y_prev_1 = y1
+    y_prev_1_cache = y_prev_1_cache.at[1, :].set(y_prev_1)
+    y_prev_0 = y0
+    y_prev_0_cache = y_prev_0_cache.at[1, :].set(y_prev_0)
+
+    for i in range(2, num_steps):
+        y_new = step(y_prev_0, y_prev_1, y_prev_2)
 
         y_prev_2 = y_prev_1
-        y_prev_1 = y
-        y = y_new
+        y_prev_2_cache = y_prev_2_cache.at[i, :].set(y_prev_2)
+        y_prev_1 = y_prev_0
+        y_prev_1_cache = y_prev_1_cache.at[i, :].set(y_prev_1)
+        y_prev_0 = y_new
+        y_prev_0_cache = y_prev_0_cache.at[i, :].set(y_prev_0)
 
-    return y
+    Dy_prev_0 = Dy
+    Dy_new = 0
+    Dy_prev_1 = 0
+    Dy_prev_2 = 0
+
+    for i in range(num_steps - 1, 1, -1):
+        Dy_new += Dy_prev_0
+        Dy_prev_0 += Dy_prev_1
+        Dy_prev_1 += Dy_prev_2
+
+        _, vjp_step = vjp(step, y_prev_0_cache[i - 1, :], y_prev_1_cache[i - 1, :], y_prev_2_cache[i - 1, :])
+        tmp0, tmp1, tmp2 = vjp_step(Dy_new)
+        Dy_prev_0 += tmp0
+        Dy_prev_1 += tmp1
+        Dy_prev_2 += tmp2
+
+    Dy0 = Dy_prev_0
+    Dy1 = Dy_prev_1
+    Dy2 = Dy_prev_2
+
+    return y_prev_0, (Dy0, Dy1, Dy2)
+
+def AB3_adm_2(f, y0, y1, y2, Dy, dt, num_steps):
+
+    @jit
+    def step(y_prev_0, y_prev_1, y_prev_2):
+        return y_prev_0 + dt * AB3_step(f, y_prev_0, y_prev_1, y_prev_2)
+
+    y_prev_2 = y2
+    y_prev_1 = y1
+    y_prev_0 = y0
+
+    for i in range(2, num_steps):
+        y_new = step(y_prev_0, y_prev_1, y_prev_2)
+        y_prev_2 = y_prev_1
+        y_prev_1 = y_prev_0
+        y_prev_0 = y_new
+
+    Dy_prev_0 = Dy
+    Dy_prev_1 = 0 
+    Dy_prev_2 = 0
+    Dy_new = 0
+
+    for i in range(num_steps-1, 1, -1):
+        # y_prev_0 = y_new
+        Dy_new += Dy_prev_0
+        # y_prev_1 = y_prev_0
+        Dy_prev_0 += Dy_prev_1
+        # y_prev_2 = y_prev_1
+        Dy_prev_1 += Dy_prev_2
+        # y_new = step(y_prev_0, y_prev_1, y_prev_2)
+        _, vjp_step = vjp(step, y_prev_0_cache[i - 1, :], y_prev_1_cache[i - 1, :], y_prev_2_cache[i - 1, :])
+        tmp0, tmp1, tmp2 = vjp_step(Dy_new)
+        Dy_prev_0 += tmp0
+        Dy_prev_1 += tmp1
+        Dy_prev_2 += tmp2
+
+    y_prev_2 = y2
+    y_prev_1 = y1
+    y_prev_0 = y0
+
+    return y_prev_0
--- solvers-jax/advection-ab3-jax.py
+++ solvers-jax/advection-ab3-jax.py
@@ -52,19 +52,53 @@ def solver_ab3_tlm(u_initial, du_initial, v, dx, num_points, dt, num_steps):
         return - v * A(u)
     
     @jit
-    def step(y):
+    def euler_step(y):
         return y + dt * f(y)
 
     dy2 = du_initial
     y2 = u_initial
 
     # y1 = y2 + dt * f(y2)
-    y1, dy1 = jvp(step, y2, dy2)
+    y1, dy1 = jvp(euler_step, (y2,), (dy2,))
     
     # y0 = y1 + dt * f(y1)
-    y0, dy0 = jvp(step, y1, dy1)
+    y0, dy0 = jvp(euler_step, (y1,), (dy1,))
     
-    return AB3_tlm(f, y0, dy0, y1, y2, dt, num_steps)
+    primal, tangent = AB3_tlm(f, y0, y1, y2, dy0, dy1, dy2, dt, num_steps)
+    return tangent
+
+def solver_ab3_adm(u_initial, Dv, v, dx, num_points, dt, num_steps):
+    A = create_advection_opeartor(dx, num_points)
+
+    @jit
+    def f(u):
+        return - v * A(u)
+    
+    @jit
+    def euler_step(y):
+        return y + dt * f(y)
+    
+    y2 = u_initial
+    y1 = y2 + dt * f(y2)
+    y0 = y1 + dt * f(y1)
+    
+    y = AB3(f, y0, y1, y2, dt, num_steps)
+
+    Dy = Dv
+
+    primal, adjoint = AB3_adm(f, y0, y1, y2, Dy, dt, num_steps)
+
+    Dy0 = adjoint[0]
+    Dy1 = adjoint[1]
+    Dy2 = adjoint[2]
+
+    _, vjp_euler_step = vjp(euler_step, y1)
+    Dy1 += vjp_euler_step(Dy0)[0]
+    _, vjp_euler_step = vjp(euler_step, y2)
+    Dy2 += vjp_euler_step(Dy1)[0]
+    Du_intial = Dy2
+
+    return Du_intial
     
 
 # Example usage
@@ -93,4 +127,84 @@ if __name__ == "__main__":
     plt.ylabel("Solution (u)")
     plt.legend()
     plt.show()
+
+    num_steps = 8
+
+    @jit
+    def m(u):
+        return solver_ab3(u, v, dx, num_points, dt, num_steps)
+
+    # @jit
+    def TLM(u, du):
+        return solver_ab3_tlm(u, du, v, dx, num_points, dt, num_steps)
+
+    # @jit
+    def ADM(u, Dv):
+        return solver_ab3_adm(u, Dv, v, dx, num_points, dt, num_steps)
+
+    def testTLMLinearity(TLM, tol):
+        N = 100
+        u0 = np.random.rand(N)
+        du = np.random.rand(N)
+        dv = np.array(TLM(jnp.array(u0), jnp.array(du)))
+        dv2 = np.array(TLM(jnp.array(u0), jnp.array(2.0*du)))
+        absolute_error = np.linalg.norm(dv2 - 2.0*dv)
+        return absolute_error < tol, absolute_error
+
+    def testTLMApprox(m, TLM, tol):
+        N = 100
+        u0 = np.random.rand(N)
+        v0 = m(jnp.array(u0))
+
+        du = np.random.rand(N)
+        dv = np.array(TLM(jnp.array(u0), jnp.array(du)))
+        
+        scale = 1.0
+
+        absolute_errors = []
+        relavite_errors = []
+        for i in range(15):
+            v1 = np.array(m(jnp.array(u0 + (scale * du))))
+            absolute_error = np.linalg.norm((scale * dv) - (v1 - v0))
+            absolute_errors.append(absolute_error)
+            relative_error = absolute_error / np.linalg.norm(v1 - v0)
+            relavite_errors.append(relative_error)
+            scale /= 10.0
+
+        # print(absolute_errors)
+        # print(relavite_errors)
+        min_relative_error = np.min(relavite_errors)
+
+        return min_relative_error < tol, min_relative_error
+
+    def testADMApprox(TLM, ADM, tol):
+        N = 100
+        u0 = np.random.rand(N)
+        du =  np.random.rand(N)
+
+        dv = np.array(TLM(jnp.array(u0), jnp.array(du)))
+
+        M = jnp.size(dv)
+        Dv =  np.random.rand(M)
+        Du = np.array(ADM(jnp.array(u0), jnp.array(Dv))).flatten()
+        
+        absolute_error = np.abs(np.dot(dv, Dv) - np.dot(du, Du))
+        return absolute_error < tol, absolute_error
+
+    print("Test TLM Linearity:")
+    success, absolute_error = testTLMLinearity(TLM, 1.0e-13)
+    print("success = ", success, ", absolute_error = ", absolute_error)
+
+    print("Test TLM Approximation:")
+    success, relative_error = testTLMApprox(m, TLM, 1.0e-13)
+    print("success = ", success, ", relative error = ", relative_error)
+
+    print("Test ADM Approximation:")
+    success, absolute_error = testADMApprox(TLM, ADM, 1.0e-13)
+    print("success = ", success, ", relative absolute_error = ", absolute_error)
+
+
+
+
+
 
